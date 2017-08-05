ace7518e14d34800665b7ee40812fc0df8f1ff90
Xuxue1 <1915998056@qq.com>
2017-08-05 10:17:41 +0800

    开启gzip

--- config/index.js
+++ config/index.js
@@ -14,7 +14,7 @@ module.exports = {
     // Before setting to `true`, make sure to:
     // npm install --save-dev compression-webpack-plugin
     productionGzip: true,
-    productionGzipExtensions: ['js', 'css'],
+    productionGzipExtensions: ['js.gz', 'css.gz'],
     // Run the build command with an extra argument to
     // View the bundle analyzer report after build finishes:
     // `npm run build --report`
