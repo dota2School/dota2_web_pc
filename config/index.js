eeffae9209cc1d1b6f3dd9009df7356d0f87f454
Xuxue1 <1915998056@qq.com>
2017-08-14 11:30:07 +0800

    修复  跨域问题

--- config/index.js
+++ config/index.js
@@ -29,7 +29,7 @@ module.exports = {
     assetsPublicPath: '/',
     proxyTable: {
       '/web': {
-        target: 'http://47.92.82.249/api/web',
+        target: 'http://47.92.82.249/web',
         changeOrigin: true,
         pathRewrite: {
           '^/web': ''
